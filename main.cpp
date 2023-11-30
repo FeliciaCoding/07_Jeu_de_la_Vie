0ab2b12e9fe34178ad1c3be1ee13c18fa2d298db
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2023-11-30 04:50:12 +0100

    Add printGrid function & Console Clear

--- main.cpp
+++ main.cpp
@@ -1,29 +1,48 @@
-
 #include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <string>
+
+#ifdef _WIN32
+#define CLEAR_SCREEN "CLS"
+#else
+#define CLEAR_SCREEN "clear"
+#endif
+
 using namespace std;
 
-// change row and column value to set the canvas size
-const int row = 5;
-const int col = 4;
+const int GAME_ROW = 9;
+const int GAME_COL = 9;
 
-// creates row boundary
-int row_line()
-{
+void printGrid(int grid[][GAME_COL]) {
+    // print column indice
+    const int WIDE_ELEMENT = 3;
+    cout << setw(WIDE_ELEMENT) << setfill(' ') << "";
+    for (int col = 0; col < GAME_COL; ++col) {
+        std::cout << right << std::setw(WIDE_ELEMENT) << col;
+    }
     cout << endl;
-    for (int i = 0; i < col; i++) {
-        cout << " -----";
+
+    // print grid with row indices
+    for (int row = 0; row < GAME_ROW; ++row) {
+        cout << right << setw(WIDE_ELEMENT) << row;
+        for (int col = 0; col < GAME_COL; ++col) {
+            char symbol = (grid[row][col] == 1) ? 'X' : '.';
+            cout << setw(WIDE_ELEMENT) << symbol;
+        }
+        cout << endl;
     }
     cout << endl;
 }
 
+
 // returns the count of alive neighbours
-int count_live_neighbour_cell(int a[row][col], int r, int c)
-{
+int count_live_neighbour_cell(int a[GAME_ROW][GAME_COL], int r, int c) {
     int i, j, count = 0;
     for (i = r - 1; i <= r + 1; i++) {
         for (j = c - 1; j <= c + 1; j++) {
             if ((i == r && j == c) || (i < 0 || j < 0)
-                || (i >= row || j >= col)) {
+                || (i >= GAME_ROW || j >= GAME_COL)) {
                 continue;
             }
             if (a[i][j] == 1) {
@@ -34,63 +53,64 @@ int count_live_neighbour_cell(int a[row][col], int r, int c)
     return count;
 }
 
-int main()
-{
-    int a[row][col], b[row][col];
+int main() {
+
+    // TODO : Introduction message (This program... )
+
+    int currentGrid[GAME_ROW][GAME_COL], nextGrid[GAME_ROW][GAME_COL];
     int i, j;
     int neighbour_live_cell;
 
     // generate matrix canvas with random values (live and
     // dead cells)
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < col; j++) {
-            a[i][j] = rand() % 2;
+    for (i = 0; i < GAME_ROW; i++) {
+        for (j = 0; j < GAME_COL; j++) {
+            currentGrid[i][j] = rand() % 2;
         }
     }
 
-    // print array matrix
-    cout << "Initial Stage:";
-    row_line();
-    for (i = 0; i < row; i++) {
-        cout << ":";
-        for (j = 0; j < col; j++) {
-            cout << "  " << a[i][j] << "  :";
-        }
-        row_line();
+// TODO : printTitles Funcitons
+    const int    GAME_WIDTH      = 30;
+    const char   TITLE_SEPERATOR = '-';
+    const char   SPACE           = ' ';
+    const string TITLE           = "GENRATION NO ";
+    const int    NUM_GENERATIONS = 1;  // TODO : Set back to 6 once the buffer grid is created
+
+    system(CLEAR_SCREEN);
+    cout << setw(GAME_WIDTH) << setfill(TITLE_SEPERATOR) << "" << endl;
+    cout << setw(9) << setfill(SPACE) << "" << TITLE << "0" << endl;
+    cout << setw(GAME_WIDTH) << setfill(TITLE_SEPERATOR) << "" << endl;
+
+    printGrid(currentGrid);
+
+
+    // calculate the subsequent generations
+    for (int gen = 1; gen <= NUM_GENERATIONS; ++gen) {
+        system(CLEAR_SCREEN);
+        cout << setw(GAME_WIDTH) << setfill(TITLE_SEPERATOR) << "" << endl;
+        cout << setw(9) << setfill(SPACE) << "" << TITLE << gen << endl;
+        cout << setw(GAME_WIDTH) << setfill(TITLE_SEPERATOR) << "" << endl;
     }
 
     // next canvas values based on live neighbour count
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < col; j++) {
+    for (i = 0; i < GAME_ROW; i++) {
+        for (j = 0; j < GAME_COL; j++) {
             neighbour_live_cell
-                    = count_live_neighbour_cell(a, i, j);
-            if (a[i][j] == 1
+                    = count_live_neighbour_cell(currentGrid, i, j);
+            if (currentGrid[i][j] == 1
                 && (neighbour_live_cell == 2
                     || neighbour_live_cell == 3)) {
-                b[i][j] = 1;
-            }
-
-            else if (a[i][j] == 0
-                     && neighbour_live_cell == 3) {
-                b[i][j] = 1;
-            }
-
-            else {
-                b[i][j] = 0;
+                nextGrid[i][j] = 1;
+            } else if (currentGrid[i][j] == 0
+                       && neighbour_live_cell == 3) {
+                nextGrid[i][j] = 1;
+            } else {
+                nextGrid[i][j] = 0;
             }
         }
     }
 
-    // print next generation
-    cout << "\nNext Generation:";
-    row_line();
-    for (i = 0; i < row; i++) {
-        cout << ":";
-        for (j = 0; j < col; j++) {
-            cout << "  " << b[i][j] << "  :";
-        }
-        row_line();
-    }
+    printGrid(nextGrid);
 
     return 0;
 }
